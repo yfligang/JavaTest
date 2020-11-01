c818a88054efbff368c6ffb0903585ba9f515a1d
LiGang <nick_hero_2004@163.com>
2020-11-01 10:01:29 +0800

    1031

--- src/com/guazi/study/DemoBasicTest.java
+++ src/com/guazi/study/DemoBasicTest.java
@@ -3,6 +3,13 @@ package com.guazi.study;
 public class DemoBasicTest {
 
     public static void main(String[] args){
-        System.out.println();
+        //字符串常量
+        System.out.println("ABC");
+
+        int num = (int)10000000000000000L;  //出现数据溢出的情况  1874919424
+        System.out.println(num);
+
+        char c = 'A'; //字符型变量
+        System.out.println(c + 100); //有一个翻译的动作
     }
 }
