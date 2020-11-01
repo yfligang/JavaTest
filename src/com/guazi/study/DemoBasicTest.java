6ec85d4931f7615a3aadeaee1ce066392fb51560
LiGang <nick_hero_2004@163.com>
2020-11-01 23:14:18 +0800

    1101

--- src/com/guazi/study/DemoBasicTest.java
+++ src/com/guazi/study/DemoBasicTest.java
@@ -11,5 +11,35 @@ public class DemoBasicTest {
 
         char c = 'A'; //字符型变量
         System.out.println(c + 100); //有一个翻译的动作
+
+        String str = "Hello";
+        System.out.println(str + 1);
+
+        int num1 = 100;
+        int result = num1--;
+        System.out.println(result); //先显示再减 100
+        System.out.println(num1); //99
+
+        int num2 = 100;
+        int result1 = --num2;
+        System.out.println(result1); //先减再显示 99
+        System.out.println(num2); //99   31 11 19
+
+        int number = 99;
+
+        switch(number){
+            case 50:
+                System.out.println("考试不及格");
+                break;
+            case 60:
+                System.out.println("考试及格");
+                break;
+            case 100:
+                System.out.println("得了满分");
+                break;
+            default:
+                System.out.println("我说了算");
+        }
+
     }
 }
