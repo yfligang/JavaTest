a2bd18306a30ab44b9f800e44edee4f10299d208
ligang2 <ligang2@liepin.com>
2021-03-27 20:20:56 +0800

    20210326

--- src/com/liepin/rim/org/day01/test05/DemoTest.java
+++ src/com/liepin/rim/org/day01/test05/DemoTest.java
@@ -1,11 +1,11 @@
 package com.liepin.rim.org.day01.test05;
 
-public class DemoTest extends EnumDemoImpl {
+public class DemoTest {
 
     public static void main(String[] args) {
         DemoTest demoTest = new DemoTest();
         demoTest.method();
-        EnumDemoImpl enumDemo = new EnumDemoImpl();
+//        EnumDemoImpl enumDemo = new EnumDemoImpl();
     }
 
     void method(){
