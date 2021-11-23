bfa2d25dba99219b6046cf30d7e1bcfe9809c703
ligang2 <ligang2@liepin.com>
2021-11-23 20:37:10 +0800

    2021112301;

--- src/com/liepin/rim/org/reverse/ReverseArray.java
+++ src/com/liepin/rim/org/reverse/ReverseArray.java
@@ -1,5 +1,30 @@
 package com.liepin.rim.org.reverse;
 
 public class ReverseArray {
+
+    static int[] array = new int[]{10,11,34,54,21,9,0,11};
+    static int[] newArray = new int[]{12,31,5,21,43};
+
+    //将数组{10,11,34,54,21,9,0,11} 原地反转
+    public static void reverse(int[] array){
+        //int[] newArray = new int[];
+        int temp;
+        for (int i = 0; i < array.length/2 ; i++) {
+            temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
+
+        for (int num : array
+             ) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void main(String[] args){
+        reverse(array);
+        System.out.println("========================");
+        reverse(newArray);
+    }
     
 }
