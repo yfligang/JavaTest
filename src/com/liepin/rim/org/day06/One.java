2eed8052b33b532bc115e5b5468e7e82ea11e4b3
ligang2 <ligang2@liepin.com>
2021-12-14 11:19:58 +0800

    2021121401

--- src/com/liepin/rim/org/day06/One.java
+++ src/com/liepin/rim/org/day06/One.java
@@ -13,7 +13,30 @@ public class One {
         }
     }
 
+    public static boolean isSame(int m,int n){
+        boolean isSame;
+        if (m == n){
+            System.out.println("数字一样大");
+            isSame = true;
+        }else {
+            isSame = false;
+            System.out.println("数字不一样大");
+        }
+
+        return isSame;
+    }
+
     public static void main(String[] args){
         firstMethod(1,2);
+        isSame(1,1);
+        secondMethod(isSame(9,10));
+    }
+
+    public static void secondMethod(boolean isSame){
+        if (isSame){
+            System.out.println("我是第一名");
+        }else {
+            System.out.println("痛苦还是一样");
+        }
     }
 }
